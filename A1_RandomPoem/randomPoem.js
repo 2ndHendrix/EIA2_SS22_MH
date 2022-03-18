74b898158b7de1bf6befd511a79cb686ad001824
Manu <manuel.hubert@hs-furtwangen.de>
2022-03-18 17:34:33 +0100

    fixed it

--- A1_RandomPoem/randomPoem.js
+++ A1_RandomPoem/randomPoem.js
@@ -1,30 +1,23 @@
+// tslint:disable-next-line: no-namespace
 var radnomizedPoemScript;
 (function (radnomizedPoemScript) {
-    var array_subjekte;
-    array_subjekte = ["Gandalf", "Gimli", "Frodo", "Eragon", "Boromir", "Golum", "Alf", "Wilmar", "Saruman"];
-    var array_prädikate;
-    array_prädikate = ["trägt", "vergisst", "klaut", "snackt", "liebt", "hasst", "riecht", "mag", "fürchtet"];
-    var array_objekte;
-    array_objekte = ["sein zweites Frühstück", "die schwarzen Reiter", "Gondor", "den einen Ring", "Orkses", "Mordor", "Vulkangestein", "Riesenspinnen", "Kriegselefanten"];
-    console.log(getVerse(array_objekte, array_prädikate, array_subjekte));
+    var subject = ["Gandalf", "Gimli", "Frodo", "Eragon", "Boromir", "Golum", "Alf", "Wilmar", "Saruman"];
+    var praedicates = ["trägt", "vergisst", "klaut", "snackt", "liebt", "hasst", "riecht", "mag", "fürchtet"];
+    var objects = ["sein zweites Frühstück", "die schwarzen Reiter", "Gondor", "den einen Ring", "Orkses", "Mordor", "Vulkangestein", "Riesenspinnen", "Kriegselefanten"];
+    // tslint:disable-next-line: no-console
+    var versComplete = getVerse(subject, praedicates, objects);
+    console.log(versComplete);
     function getVerse(_sub, _prä, _obj) {
         var versAufnahme = " ";
-        var _random = Math.random() * _sub.length;
-        var _random = Math.floor(_random);
-        var result = _random;
-        var wordPick = array_subjekte.splice(result);
+        var _random1 = Math.floor(Math.random() * _sub.length);
+        var wordPick = subject.splice(_random1);
         versAufnahme = versAufnahme + wordPick[0] + " ";
-        var _random = Math.random() * _prä.length;
-        var _random = Math.floor(_random);
-        var result = _random;
-        var wordPick = array_prädikate.splice(result);
-        versAufnahme = versAufnahme + wordPick[0] + " ";
-        var _random = Math.random() * _obj.length;
-        var _random = Math.floor(_random);
-        var result = _random;
-        var wordPick = array_objekte.splice(result);
-        versAufnahme = versAufnahme + wordPick[0];
-        console.log(result);
+        var _random2 = Math.floor(Math.random() * _sub.length);
+        var wordPick2 = subject.splice(_random2);
+        versAufnahme = versAufnahme + wordPick2[0] + " ";
+        var _random3 = Math.floor(Math.random() * _sub.length);
+        var wordPick3 = subject.splice(_random3);
+        versAufnahme = versAufnahme + wordPick3[0] + " ";
         return versAufnahme;
     }
 })(radnomizedPoemScript || (radnomizedPoemScript = {}));
