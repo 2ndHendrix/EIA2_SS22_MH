7bc04aae84854ad2b97aab49cf9fe838a83f8d2e
Manu <manuel.hubert@hs-furtwangen.de>
2022-07-13 02:13:21 +0200

    some progress

--- A_Endabgabe_MH_271345/main.ts
+++ A_Endabgabe_MH_271345/main.ts
@@ -1,15 +1,19 @@
 namespace GardensSimulator {
+    window.addEventListener("load", hndload);
 
-
+    export let context: CanvasRenderingContext2D;
+    let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
     let startGameForm: HTMLFormElement;
     let startSimulationButton: HTMLButtonElement;
+
+
+
     let main: HTMLDivElement;
     let farm: Farm[] = [];
+    let field: Field[] = [];
     let priceConsumable: number = Math.round(Math.random() * 100);
 
 
-    window.addEventListener("load", hndload);
-    export let context: CanvasRenderingContext2D;
 
     function hndload(_event: Event): void {
 
@@ -25,7 +29,38 @@ namespace GardensSimulator {
 
         startSimulationButton.addEventListener("click", startGame);
 
+
+
+
+
+        // Buttons for Panting veggies
+        let broccoliButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#broccoli");
+        let cabbageButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#cabbage");
+        let cornButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#corn");
+        let carrotButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#carrot");
+        let spinachButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#spinach");
+
+        broccoliButton.addEventListener("click", plantingVeggies);
+        cabbageButton.addEventListener("click", plantingVeggies);
+        cornButton.addEventListener("click", plantingVeggies);
+        carrotButton.addEventListener("click", plantingVeggies);
+        spinachButton.addEventListener("click", plantingVeggies);
+
+        // Buttons for consumables
+        let pesticidesButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#buttonPesticides");
+        let waterButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#buttonWater");
+        let fertilizerButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#buttonFertilizer");
+
+        pesticidesButton.addEventListener("click", usingConsumables);
+        waterButton.addEventListener("click", usingConsumables);
+        fertilizerButton.addEventListener("click", usingConsumables);
+
+
+
+        // canvas.addEventListener("mousedown", clickField);
+
     }
+
 
     function startGame(_event: Event): void {
         let startGameMenu: HTMLDivElement = <HTMLDivElement>document.querySelector("#startGameMenu");
@@ -34,12 +69,37 @@ namespace GardensSimulator {
         main.style.display = "";
 
         buildFields();
-        //updateFarm();
+        updateFarm();
         getFunds();
+        getAmountConsumables();
         setInterval(updatePrices, 1000);
-       // setInterval(updateFarm)
+        // setInterval(updateFarm)
 
     }
+
+    function plantingVeggies(_event: Event): void {
+
+        console.log("button clicked");
+    }
+
+    function usingConsumables(_event: Event): void {
+        console.log("Using consumables");
+    }
+
+    function getAmountConsumables(): void {
+        let numberPesticides: HTMLInputElement = <HTMLInputElement>document.getElementById("setPesticides");
+        let amountPesticides: number = parseInt(numberPesticides.value);
+        document.querySelector("#amountPesticides").innerHTML = amountPesticides + "";
+        console.log(amountPesticides);
+
+    }
+
+    // function clickField(_event: MouseEvent): void {
+
+    //     let fieldButto: HTMLDivElement = <HTMLDivElement>document.getElementById("row");
+    //     // let fieldClicked: Position = new Position(_event.clientX - context.canvas.offsetLeft, _event.clientY - context.canvas.offsetTop);
+    //     //console.log(fieldClicked);
+    // }
 
     function updatePrices(): void {
         setWorthHarvest();
@@ -72,22 +132,17 @@ namespace GardensSimulator {
 
         let priceRangeLow: HTMLInputElement = <HTMLInputElement>document.getElementById("priceRangeLow");
         let inputLow: number = parseInt(priceRangeLow.value);
-        let priceRangeHigh: HTMLInputElement = <HTMLInputElement>document.querySelector("#priceRangeHigh");
+        let priceRangeHigh: HTMLInputElement = <HTMLInputElement>document.getElementById("priceRangeHigh");
         let inputHigh: number = parseInt(priceRangeHigh.value);
 
         let priceNew: number;
 
-        if (inputHigh < priceConsumable) {
-            priceConsumable = priceNew - 1;
+        priceNew = Math.floor(Math.random() * (inputHigh - inputLow + 1)) + inputLow;
 
-        }
+        document.querySelector("#pricePesticides").innerHTML = priceNew + "";
+        document.querySelector("#priceFertilizer").innerHTML = priceNew + "";
 
-        document.querySelector("#pricePesticides").innerHTML = priceConsumable + "";
-        document.querySelector("#priceFertilizer").innerHTML = priceConsumable + "";
-
-
-
-        console.log(inputLow);
+        console.log(priceNew);
     }
 
     function hndchange(_event: Event): void {
@@ -97,24 +152,39 @@ namespace GardensSimulator {
     }
 
     function buildFields(): void {
-        let field: Field = new Field();     // Instanzierung von field
-        farm.push(field);                   // Aufrufen von field aus Farm
+
+        for (let index: number = 0; index <= 4; index++) {
+            document.querySelector("row" + index);
+            let rows: HTMLDivElement = <HTMLDivElement>document.getElementById("row" + index);
+
+
+            for (let index: number = 0; index <= 7; index++) {
+                let fieldRow: HTMLDivElement = <HTMLDivElement>document.createElement("div");
+                fieldRow.classList.add("field");
+                rows.appendChild(fieldRow);
+
+
+                let field: Field = new Field();
+                farm.push(field);
+
+                // let stateAd: HTMLDivElement = <HTMLDivElement>document.createElement("div");
+                // stateAd.classList.add("state");
+                // field.appendChild(stateAd);
+            }
+        }
 
 
     }
 
-    // function updateFarm(): void {
-    //     for (let farms of farm) {
-    //         farms.build();
-    //         farms.water();
-    //         farms.fertilize();
-    //         farms.pesticide();
+    function updateFarm(): void {
+        for (let farms of farm) {
+            farms.build();
+            farms.water();
+            farms.fertilize();
+            farms.pesticide();
 
-    //     }
-
-
-    // }
+        }
 
 
-
+    }
 }
